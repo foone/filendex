e1e1838bd6aa2d412d947e827568159799010c8f
Foone Turing <foone.turing@purestorage.com>
2017-05-12 17:18:09 -0700

    Recursive searching implemented (mostly)

--- walk.py
+++ walk.py
@@ -5,6 +5,7 @@ DATABASE_PATH = 'filendex.sqlite3'
 BUFFER_SIZE = 1024*1024 # TODO: configure on command line?
 HASHERS='md5 sha1 sha256'.split()
 ARCHIVE_EXTS = 'zip iso'.split()
+MAXDEPTH = 100
 
 EMPTYFILE={}
 for algo in HASHERS:
@@ -41,8 +42,11 @@ def record_file(row):
 		','.join('?'*len(row))
 	)
 	try:
-		with conn as cur:
+		cur = conn.cursor()
+		with conn:
 			cur.execute(sql, row.values())
+			cur.close()
+		return cur.lastrowid
 	except sqlite3.IntegrityError:
 		pass # Silently skip missing files
 		# TODO: add an option to print these files or error?
@@ -56,7 +60,7 @@ def setup_database():
 		except sqlite3.OperationalError:
 			with open('create.sql','rb') as f:
 				cur.executescript(f.read())
-
+	return conn
 
 def is_archive(row):
 	if row['ext'] in ARCHIVE_EXTS:
@@ -64,19 +68,29 @@ def is_archive(row):
 	# TODO: check ISO
 	return False
 
-def handle_archive(row, dirnum):
-	new_dirnum = dirnum + 1
-	# TODO: extract to tmpfs
+def handle_archive(fileid, path, dirnum):
+	target_dir = os.path.join('temp', '{:04}'.format(dirnum))
+	os.mkdir(target_dir)
+	subprocess.check_call(['unar', '-o', target_dir, '-D', '-q', path])
 
-def scan_directory(top_path, dirnum, inside):
+	scan_directory(target_dir, dirnum, fileid)
 
+def scan_directory(top_path, dirnum=0, inside=None):
 	for dirpath, dirnames, filenames in os.walk(top_path):
+		if inside is not None:
+			# This is messy, but we need to remove temp/0001
+			#Normally and remove components?
+			savepath = dirpath[len(top_path)+1:]
+		else:
+			savepath = dirpath
+
 		for filename in filenames:
 			row = {
 				'inside': inside
 			}
-			row['path'] = path = os.path.join(dirpath, filename)
-			print path
+			path = os.path.join(dirpath, filename)
+			row['path'] = os.path.join(savepath, filename)
+			print row['path']
 			row['ext'] = os.path.splitext(path)[1].lower().lstrip('.')
 			# TODO: Precheck files (locally or remotely?) before hashing, so we don't waste time rehashing
 			info = os.lstat(path)
@@ -94,22 +108,25 @@ def scan_directory(top_path, dirnum, inside):
 			row['file_type'] = get_file_type(path)
 
 			try:
-				record_file(row)
+				fileid = record_file(row)
 			except:
 				print row
 				raise
 
 			if is_archive(row):
-				print 'IS ARCHIVE! RECURSING'
-				handle_archive(row, dirnum)
+				if dirnum+1 == MAXDEPTH:
+					print 'NOT RECURSING, REACHED MAX DEPTH'
+				else:
+					print 'IS ARCHIVE! RECURSING'
+					handle_archive(fileid, path, dirnum+1)
 
 
 
 
-if __name __ == '__main__':
+if __name__ == '__main__':
 
 	conn = setup_database()
 
 	basepath = os.path.abspath(sys.argv[1])
 
-	scan_directory(basepath, 0, None)
+	scan_directory(basepath)
